0a98cdd1a0182ea3a5bade3a7c00316d0db7b794
xeèpanunty <xepanunty@gmail.com>
2020-09-11 13:27:43 +0200

    Base of page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -546,11 +546,11 @@
 
                         <div class="row form-group">
                             <div class="col-md-6 mb-3 mb-md-0">
-                                <label class="text-black" for="fname">First Name</label>
+                                <label class="text-black" for="fname">{{ trans('app.fristname') }}</label>
                                 <input type="text" id="fname" class="form-control">
                             </div>
                             <div class="col-md-6">
-                                <label class="text-black" for="lname">Last Name</label>
+                                <label class="text-black" for="lname">{{ trans('app.lastname') }}</label>
                                 <input type="text" id="lname" class="form-control">
                             </div>
                         </div>
@@ -558,7 +558,7 @@
                         <div class="row form-group">
 
                             <div class="col-md-12">
-                                <label class="text-black" for="email">Email</label>
+                                <label class="text-black" for="email">{{ trans('app.email_contact') }}</label>
                                 <input type="email" id="email" class="form-control">
                             </div>
                         </div>
@@ -566,22 +566,22 @@
                         <div class="row form-group">
 
                             <div class="col-md-12">
-                                <label class="text-black" for="subject">Subject</label>
+                                <label class="text-black" for="subject">{{ trans('app.subject') }}</label>
                                 <input type="subject" id="subject" class="form-control">
                             </div>
                         </div>
 
                         <div class="row form-group">
                             <div class="col-md-12">
-                                <label class="text-black" for="message">Message</label>
+                                <label class="text-black" for="message">{{ trans('app.message') }}</label>
                                 <textarea name="message" id="message" cols="30" rows="7" class="form-control"
-                                          placeholder="Write your notes or questions here..."></textarea>
+                                          placeholder="{{ trans('app.message_notes') }}"></textarea>
                             </div>
                         </div>
 
                         <div class="row form-group">
                             <div class="col-md-12">
-                                <input type="submit" value="Send Message" class="btn btn-primary btn-md text-white">
+                                <input type="submit" value="{{ trans('app.send_message') }}" class="btn btn-primary btn-md text-white">
                             </div>
                         </div>
 
@@ -595,8 +595,8 @@
 
     <div class="site-section text-center">
         <div class="container">
-            <h2 class="text-black mb-3">Join over 1,700 companies that trust us.</h2>
-            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h2 class="text-black mb-3">{{ trans('app.join') }}</h2>
+            <p class="mb-5">{{ trans('app.focus') }}</p>
             <p><a href="#" class="btn btn-primary">{{ trans('app.request_demo') }}</a></p>
         </div>
     </div>
@@ -607,24 +607,23 @@
                 <div class="col-md-9">
                     <div class="row">
                         <div class="col-md-5">
-                            <h2 class="footer-heading mb-4">About Us</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam
-                                voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
+                            <h2 class="footer-heading mb-4">{{ trans('app.about_us') }}</h2>
+                            <p>{{ trans('app.about_us_desc') }}</p>
                         </div>
                         <div class="col-md-3 ml-auto">
-                            <h2 class="footer-heading mb-4">Quick Links</h2>
+                            <h2 class="footer-heading mb-4">{{ trans('app.links') }}</h2>
                             <ul class="list-unstyled">
-                                <li><a href="#about-section" class="smoothscroll">Terms</a></li>
-                                <li><a href="#about-section" class="smoothscroll">Policy</a></li>
-                                <li><a href="#about-section" class="smoothscroll">About Us</a></li>
-                                <li><a href="#services-section" class="smoothscroll">Services</a></li>
-                                <li><a href="#testimonials-section" class="smoothscroll">Testimonials</a></li>
-                                <li><a href="#contact-section" class="smoothscroll">Contact Us</a></li>
-                                <li><a href="{{ backpack_url() }}" class="smoothscroll">Login</a></li>
+                                <li><a href="#about-section" class="smoothscroll">{{ trans('app.Terms') }}</a></li>
+                                <li><a href="#about-section" class="smoothscroll">{{ trans('app.Policy') }}</a></li>
+                                <li><a href="#about-section" class="smoothscroll">{{ trans('app.About') }}</a></li>
+                                <li><a href="#services-section" class="smoothscroll">{{ trans('app.Services') }}</a></li>
+                                <li><a href="#testimonials-section" class="smoothscroll">{{ trans('app.Testimonials') }}</a></li>
+                                <li><a href="#contact-section" class="smoothscroll">{{ trans('app.Contact') }}</a></li>
+                                <li><a href="{{ backpack_url() }}" class="smoothscroll">{{ trans('app.Login') }}</a></li>
                             </ul>
                         </div>
                         <div class="col-md-3 footer-social">
-                            <h2 class="footer-heading mb-4">Follow Us</h2>
+                            <h2 class="footer-heading mb-4">{{ trans('app.FollowUs') }}</h2>
                             <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
                             <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
                             <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
@@ -633,13 +632,13 @@
                     </div>
                 </div>
                 <div class="col-md-3">
-                    <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+                    <h2 class="footer-heading mb-4">{{ trans('app.SubscribeNewsletter') }}</h2>
                     <form action="#" method="post" class="footer-subscribe">
                         <div class="input-group mb-3">
                             <input type="text" class="form-control border-secondary bg-transparent"
                                    placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
                             <div class="input-group-append">
-                                <button class="btn btn-primary text-black" type="button" id="button-addon2">Send
+                                <button class="btn btn-primary text-black" type="button" id="button-addon2">{{ trans('app.send') }}
                                 </button>
                             </div>
                         </div>
