4cba12beb2b71cdf8592f54a7518e9593af213c7
xeèpanunty <xepanunty@gmail.com>
2020-10-15 18:23:20 +0200

    Register all with permissions diferent permissions

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -7,13 +7,11 @@
 //PROFESSIONAL
 //SUPERADMIN-XEPANUNTY
 $role = backpack_user()->roles[0]->name;
-$user2 = backpack_user();
-
-$user2->hasRole('writer');;
 
 //dd($user2->hasAllRoles(\Backpack\PermissionManager\app\Models\Role::all()));
 ?>
 
+<!--SUPERADMIN-XEPANUNTY-->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="nav-icon la la-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 <?php if($role === "SUPERADMIN-XEPANUNTY") : ?>
     <li class="nav-title">{{ trans('app.frontend_manager') }} </li>
@@ -61,6 +59,42 @@ $user2->hasRole('writer');;
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('menu-item') }}"><i class="nav-icon la la-list"></i> <span>Menu</span></a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="nav-icon la la-shopping-cart"></i> <span>Products</span></a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('tag') }}"><i class="nav-icon la la-tag"></i> <span>Tags</span></a></li>
-
     <?php endif; ?>
+<?php endif; ?>
+<!--ENTERPRISE-->
+<?php  if (in_array($role, ['PROFESSIONAL', 'STARTER', 'FREE', "ENTERPRISE"])) : ?>
+<li class="nav-title">{{ trans('app.role_'.$role) }} </li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('account-manager') }}"><i class="nav-icon las la-user-tie"></i> <span>{{ trans('app.account_manager') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('social-networks') }}"><i class="nav-icon lab la-facebook"></i> <span>{{ trans('app.manager_social_networks_2') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="nav-icon lab la-product-hunt"></i> <span>{{ trans('app.manager_product_services') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('documents') }}"><i class="nav-icon lab la-dochub"></i> <span>{{ trans('app.manager_documents') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('shop-preview') }}"><i class="nav-icon las la-store"></i> <span>{{ trans('app.manage_shop_preview') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('notifications') }}"><i class="nav-icon lar la-envelope-open"></i> <span>{{ trans('app.manage_notifications') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('ads-posts') }}"><i class="nav-icon lab la-chromecast"></i> <span>{{ trans('app.manage_ads_posts') }}</span></a></li>
+<?php if($role === "ENTERPRISE") : ?>
+<li class="nav-title">{{ trans('app.partners_clients') }} </li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('my-clients') }}"><i class="nav-icon las la-users"></i> <span>{{ trans('app.manage_clients') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manager-partners') }}"><i class="nav-icon las la-user-plus"></i> <span>{{ trans('app.manager_partners') }}</span></a></li>
+<li class="nav-title">{{ trans('app.manage_shop_store') }} </li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('personalize-shop') }}"><i class="nav-icon las la-shopping-bag"></i> <span>{{ trans('app.personalize_shop') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-orders') }}"><i class="nav-icon  las la-receipt"></i> <span>{{ trans('app.manage_payments') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-tracking-codes') }}"><i class="nav-icon  las la-network-wired"></i> <span>{{ trans('app.manage_tracking') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('payment-methods') }}"><i class="nav-icon las la-money-check"></i> <span>{{ trans('app.manage_shipping_methods') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('shipping-methods') }}"><i class="nav-icon las la-campground"></i> <span>{{ trans('app.manage_payment_methods') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('personalize-invoices') }}"><i class="nav-icon las la-file-invoice"></i> <span>{{ trans('app.manage_personalize_invoices') }}</span></a></li>
+<li class="nav-title">{{ trans('app.manage_others') }} </li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-email') }}"><i class="nav-icon las la-envelope"></i> <span>{{ trans('app.manage_email') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('sub-domain') }}"><i class="nav-icon lab la-slack-hash"></i> <span>{{ trans('app.manage_subdomain') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-integrations') }}"><i class="nav-icon lab la-connectdevelop"></i> <span>{{ trans('app.manage_integrations') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-newsletters') }}"><i class="nav-icon las la-mail-bulk"></i> <span>{{ trans('app.manage_shop_clients_newsletters') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('buy-domain') }}"><i class="nav-icon lab la-wikipedia-w"></i> <span>{{ trans('app.more_by_domain') }}</span></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('manage-integrations-others') }}"><i class="nav-icon lab la-telegram"></i> <span>{{ trans('app.manage_integrations_2') }}</span></a></li>
+<?php endif; ?>
+<?php  if (in_array($role, ['PROFESSIONAL', 'STARTER', 'FREE', "ENTERPRISE"])) : ?>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('support-packages') }}"><i class="nav-icon lab la-rocketchat"></i> <span>{{ trans('app.support') }}</span></a></li>
+<?php endif; ?>
+<?php endif; ?>
+
+<?php  if (in_array($role, ['SALES'])) : ?>
+<li class="nav-title">{{ trans('app.role_'.$role) }} </li>
 <?php endif; ?>
